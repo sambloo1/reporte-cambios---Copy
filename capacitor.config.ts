17842cad1c3ad4431b5945bf25347cc8cf3b132b
sambloo1 <samerazocar@gmail.com>
2025-06-25 18:43:57 -0400

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.miempresa.gestoroperaciones',
+  appName: 'GestordeOperaciones',
+  webDir: 'build'
+};
+
+export default config;
